e063dbde91f066f8fb529ffafe90619e5b920328
unknown <mrunalmetkar@gmail.com>
2022-08-22 21:26:56 +0530

    Adding code to find number is prime/not

---
+++ ModofNumber.java
@@ -0,0 +1,17 @@
+
+//Program to find mod of a number
+
+class Solution {
+    public int absolute(int I) {
+        // code here
+        
+        if(I>=0)
+            return I;
+        
+        if(I<0)
+            return I-I-I;
+    
+    return I;    
+    }
+    
+}
